e1c33d549c55c25ad006f31f147fbaf269ce12af
Marcovic Repo <hicsuntmarcovic@gmail.com>
2023-06-09 10:03:50 +0200

    Update streamlit_template.py

--- streamlit_template.py
+++ streamlit_template.py
@@ -4,9 +4,9 @@ from langchain.llms import OpenAI
 st.title('🦜🔗 Quickstart App')
 
 
-#openai_api_key = st.secrets["OPENAI_API_KEY"]
+openai_api_key = st.secrets["OPENAI_API_KEY"]
 
-openai_api_key = st.sidebar.text_input('OpenAI API Key')
+#openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
 def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
@@ -16,9 +16,9 @@ with st.form('my_form'):
   text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
   submitted = st.form_submit_button('Submit')
   if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-     if submitted and openai_api_key.startswith('sk-'):
-         generate_response(text)
+      st.warning('Please enter your OpenAI API key!', icon='⚠')
+  if submitted:
+      generate_response(text)
     #if submitted:
         #generate_response(text)
    
